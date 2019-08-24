577058e1635fa511d14cf33b2cd624115b871d58
lihenan123 <446607781@qq.com>
2019-08-24 13:23:24 +0800

    webpack

---
+++ webpack/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry:{
+        index:'./src/index.js'
+    },
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:'[name].js'
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            minify:{
+                removeAttributeQuotes:true
+            },
+            hash:true,
+            template:'./src/index.html'
+        })
+    ],
+    devServer:{
+        contentBase:path.resolve(__dirname,'dist'), //根目录
+        host:'localhost' 
+    }
+}
