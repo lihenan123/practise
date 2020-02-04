82f49dcc7c55a40e172d61cf11fc10bd3b8e12f4
lihenan123 <446607781@qq.com>
2020-02-04 20:41:44 +0800

    vue cli

--- vueapp/src/store.js
+++ vueapp/src/store.js
@@ -5,12 +5,30 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+      count:0,
+      num:2
   },
   mutations: {
-
+      //count   += num
+      add(state,num){
+          state.count+=num;
+      },
+      // count--
+      reduce(state){
+          state.count--;
+      }
+  },
+  getters:{
+      sum(state){
+          return state.count*state.num;
+      }
   },
   actions: {
-
+      addAction({commit}){
+          setTimeout(()=>{
+              //最后调用mutations中的方法修改state
+              commit('add',100);
+          },1000)
+      }
   },
 });
--- vueapp/src/views/Home.vue
+++ vueapp/src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    count的值是：{{$store.state.count}}
+    num的值是：{{$store.state.num}}
+    <!-- {{count}} -->
+    <!-- sum的值是：{{$store.getters.sum}} -->
+    sum的值是：{{sum}}
+    <!-- <button @click="$store.commit('add',5)">add</button> -->
+    <button @click="add(5)">add</button>
+    <button @click="reduce()">reduce</button>
+    <button @click="addAction()">异步add</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import {mapState,mapMutations,mapGetters,mapActions} from 'vuex';
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
+    name: 'home',
+    components: {
+      HelloWorld,
+    },
+    computed:{
+      ...mapState(['count']),
+      ...mapGetters(['sum'])
+    },
+    methods:{
+      ...mapMutations(['add','reduce']),
+      ...mapActions(['addAction'])
+    }
 };
 </script>
+
+
