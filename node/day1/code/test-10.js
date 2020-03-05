1ddbf854a7428ba05a354db52cf723471b7c2b22
lihenan123 <446607781@qq.com>
2020-03-05 14:59:36 +0800

    node

--- node/day1/code/test-10.js
+++ node/day1/code/test-10.js
@@ -2,28 +2,28 @@
 //nodejs模块 三种  原生模块  第三方模块 自定义模块 mysql request formidable
 
 //模块加载 原生模块 第三方模块 加载时不需要写路径 自定义模块需要
-//是不是所有的原生模块都需要用require加载？ global timer buffer module 
-//process stream console
-/*
+// 是不是所有的原生模块都需要用require加载？ global timer buffer module 
+// process stream console
+
 //同步
 var fs=require("fs");
-var data=fs.readFileSync("./1.txt","utf-8");
+var data=fs.readFileSync("./1.txt");
 console.log(data);
-console.log("end");*/
+console.log("end");
 
 //读取文件的两种方式
 //按照缓冲区方式读
 //按照stream的pipe的方式读  include “stdio.h”;  include "iostream.h"
 
 //异步
-var fs=require("fs");
-fs.readFile("./1.txt","utf-8",function(err,data){
-	if(err){
-		throw err;
-	}else{
-		console.log(data);
-	}
-})
+// var fs=require("fs");
+// fs.readFile("./1.txt","utf-8",function(err,data){
+// 	if(err){
+// 		throw err;
+// 	}else{
+// 		console.log(data);
+// 	}
+// })
 
 console.log("end");
 
--- node/day1/code/test-11.js
+++ node/day1/code/test-11.js
@@ -1,5 +1,7 @@
-//dns解析  递归 迭代 ip  ipv4 ipv6 hosts
-/*
+//举例异步
+//dns解析  递归 迭代 
+//ip  ipv4 ipv6 hosts
+
 var dns=require("dns");
 var http=require("http");
 urls=['http://www.qq.com','http://www.baidu.com','http://www.sohu.com']
@@ -15,7 +17,7 @@ function fetchPage(url){
 
 for(var i=0;i<urls.length;i++){
 	fetchPage(urls[i]);
-}*/
+}
 
 
 var dns=require("dns");
