aca0dce842057bac0c9b76cbbb13dfe4b81980ad
lihenan123 <446607781@qq.com>
2020-03-20 09:33:14 +0800

    node

--- node/day4/practise/blog/routes/index.js
+++ node/day4/practise/blog/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var user = require("../controllers/user.js");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/test',function(req,res,next){
+  var arr=["sun","moon","mars"];
+  res.render('aaa',{good:arr});
+});
+
+router.get('/reg',user.reg);
 
 module.exports = router;
