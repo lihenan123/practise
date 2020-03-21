aeb0c2394a531d176e884df67044d0de508ef29d
lihenan123 <446607781@qq.com>
2020-03-21 16:48:20 +0800

    node

--- node/day4/practise/blog/controllers/blog.js
+++ node/day4/practise/blog/controllers/blog.js
@@ -1,12 +1,14 @@
 var Blog_model = require("../models/blog_model.js");
+
 exports.index = function (req, res, next) {
     var uid = req.session.id;
     Blog_model.show_articles_by_name(uid,function(err,data){
         if(data.length>0){
-            res.render("/index_logined",{
+            // console.log(data);
+            res.render("index_logined",{
                 'sess':req.session,
                 'blogs':data
-            })
+            });
         }
     })
 }
